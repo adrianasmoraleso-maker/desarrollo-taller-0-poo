4299d2f431e4906ed186c567f4712fc648c5d680
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 20:28:24 -0500

    chore/Desarrollo ejercicio 3 clase producto con atributos y metodo mostrar producto

---
+++ src/main/java/org/example/Ejercicio3.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ejercicio3 {
+
+    public class Producto{
+        String nombre;
+        int precio;
+        int stock;
+
+        void mostrarProducto(){
+
+            System.out.println("Nombre del producto: "+ nombre);
+            System.out.println("Precio: "+ precio);
+            System.out.println("Stock: "+ stock);
+        }
+    }
+}
