3ad157e66bbe467dcc96b8fb86dc12a4a40b22ef
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 20:53:54 -0500

    chore/Clase cuenta bancaria con atributos y metodo para mostrar la cuenta

---
+++ src/main/java/org/example/Ejercicio4.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ejercicio4 {
+
+    public class CuentaBancaria{
+        int numero;
+        String titular;
+        int saldo;
+
+        void mostrarCuenta(){
+
+            System.out.println("Numero de cuenta: "+ numero);
+            System.out.println("Titular: "+ titular);
+            System.out.println("Saldo: "+ saldo);
+        }
+    }
+}
