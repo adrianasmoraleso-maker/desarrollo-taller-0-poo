5192e90b6441dab1744144739099e2eda5d66db1
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 19:50:42 -0500

    chore/ Ejercicio 1 Clase estudiante con atributos y metodo mostrarInfo

---
+++ src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ejercicio1 {
+
+    public class Estudiante{
+        String nombre;
+        int codigo;
+        int semestre;
+
+        void mostrarInfo(){
+
+            System.out.println("Nombre del estudiante: "+ nombre);
+            System.out.println("Codigo: "+ codigo);
+            System.out.println("Semestre: "+ semestre);
+        }
+    }
+}
