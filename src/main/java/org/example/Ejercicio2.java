774647c389d829560f35141c4bc4f085b1256c2b
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 20:09:30 -0500

    chore/Clase Vehiculo con atributos y metodo mostrar estado

---
+++ src/main/java/org/example/Ejercicio2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ejercicio2 {
+
+    public class Vehiculo{
+        String marca;
+        String modelo;
+        int velocidadActual;
+
+        void mostrarEstado(){
+
+            System.out.println("Marca del carro: "+ marca);
+            System.out.println("Modelo del carro: "+ modelo);
+            System.out.println("Velocidad actual: "+ velocidadActual);
+        }
+    }
+}
