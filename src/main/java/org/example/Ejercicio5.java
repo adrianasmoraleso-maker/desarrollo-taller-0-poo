ebed7c75c00fbe70fbf095babe0c7d7c1c4eb6d4
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 21:00:05 -0500

    chore/Clase mascota con atributos y metodo mostrar mascota

---
+++ src/main/java/org/example/Ejercicio5.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ejercicio5 {
+
+    public class Mascota{
+        String nombre;
+        String especie;
+        int edad;
+
+        void mostrarMascota(){
+
+            System.out.println("Nombre de la mascota: "+ nombre);
+            System.out.println("Especie: "+ especie);
+            System.out.println("Edad: "+ edad);
+        }
+    }
+}
