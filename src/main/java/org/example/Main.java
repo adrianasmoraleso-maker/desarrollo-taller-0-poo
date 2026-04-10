e768590367219283a275c8e6760791432d899e94
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-09 19:11:57 -0500

    chore/archivos de nuevo repositorio git taller0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, esta es una prueba para verificar que todo este funcionando bien :D!");
+    }
+}
